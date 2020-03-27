d4ccbbfdbd32c33c0ca4078ee42fea1e8f6ba065
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-27 10:42:17 +0100

    Fixed drawing

--- src/view/product/PhotoProductView.js
+++ src/view/product/PhotoProductView.js
@@ -17,24 +17,15 @@ export class PhotoProductView {
     let commentContainer = elementCreater('div', [{}], '');
 
     this.drawCanvas = elementCreater('canvas', [{}], '');
+    this.drawCanvas.setAttribute('width', 300);
+    this.drawCanvas.setAttribute('height', 300);
 
     if (Array.isArray(this.product.drawings)) {
       this.drawings = this.product.drawings;
     }
 
     if (this.product.image) {
-      let ctx = this.drawCanvas.getContext("2d");
-      let image = new Image();
-      image.onload = () => {
-        ctx.drawImage(image, 5, 5);
-        for (let i = 0; i < this.drawings.length; i++) {
-          let drawing = this.drawings[i];
-          ctx.fillStyle = "green";
-          ctx.fillRect(drawing.x, drawing.y, 5, 5);
-        }
-      }
-      image.src = this.product.image;
-      this.addDrawAction();
+      this.addPhoto();
     }
     let form = formGenerator([{
       'labelText': 'Foto uploaden',
@@ -42,9 +33,12 @@ export class PhotoProductView {
       'type': 'file',
       'class': 'file_upload form-control'
     }], () => {
+        this.drawings = [];
+        this.product.drawings = this.drawings;
         let uploadField = form.querySelector('.file_upload');
         this.mainController.saveProductUpload(this.product, uploadField);
-        this.addDrawAction();
+        console.log(this.product);
+        this.addPhoto();
     }, true);
 
     let commentForm = formGenerator([{
@@ -71,11 +65,24 @@ export class PhotoProductView {
     this.container = modal.present();
   }
 
+  addPhoto() {
+    let ctx = this.drawCanvas.getContext("2d");
+    ctx.clearRect(0, 0, 300, 300);
+    let image = new Image();
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, 300, 300);
+      for (let i = 0; i < this.drawings.length; i++) {
+        let drawing = this.drawings[i];
+        ctx.fillStyle = "green";
+        ctx.fillRect(drawing.x, drawing.y, 5, 5);
+      }
+      this.addDrawAction();
+    }
+    image.src = this.product.image;
+  }
+
   addDrawAction() {
     let ctx = this.drawCanvas.getContext("2d");
-
-    this.drawCanvas.setAttribute('width', 300);
-    this.drawCanvas.setAttribute('height', 300);
     let canDraw = false;
     this.drawCanvas.addEventListener('mousedown', e => {
       canDraw = true;
@@ -89,7 +96,7 @@ export class PhotoProductView {
     this.drawCanvas.addEventListener('mousemove', e => {
       if (canDraw) {
         let target = event.target;
-        var rect = target.getBoundingClientRect();
+        let rect = target.getBoundingClientRect();
         let x = e.clientX - rect.left;
         let y = e.clientY - rect.top;
         ctx.fillStyle = "green";
